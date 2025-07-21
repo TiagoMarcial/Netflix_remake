ee24c89f8d380943b8081880e16618f0a561d2de
Tiago Marcial <tiko127@hotmail.com>
2025-07-21 14:54:15 -0300

    feat: Adiciona RecyclerView à tela inicial.

--- app/src/main/java/dev/tiagomarcial/netflix_remake/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/netflix_remake/MainActivity.kt
@@ -1,20 +1,46 @@
 package dev.tiagomarcial.netflix_remake
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val adapter = MainAdapter()
+        val rv: RecyclerView =  findViewById(R.id.rv_main)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+    }
+
+    private inner class MainAdapter: RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+            val view = layoutInflater.inflate(R.layout.movie_item, parent, false)
+            return MovieViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 60
+        }
+
+        override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        private inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         }
     }
 }
