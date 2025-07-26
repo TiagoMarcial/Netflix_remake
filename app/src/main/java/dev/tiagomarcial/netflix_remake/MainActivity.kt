2c8a67b7763ec13c40a5c28cc3d4bb5c12fe580b
Tiago Marcial <tiko127@hotmail.com>
2025-07-26 20:55:08 -0300

    Adiciona tela de filmes

--- app/src/main/java/dev/tiagomarcial/netflix_remake/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/netflix_remake/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.tiagomarcial.netflix_remake
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
--- app/src/main/java/dev/tiagomarcial/netflix_remake/MovieActivity.kt
+++ app/src/main/java/dev/tiagomarcial/netflix_remake/MovieActivity.kt
@@ -1,6 +1,7 @@
 package dev.tiagomarcial.netflix_remake
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -9,15 +10,15 @@ import androidx.core.view.WindowInsetsCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_movie)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.outline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
