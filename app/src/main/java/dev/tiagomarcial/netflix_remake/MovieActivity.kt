86601f0b031fa0ebae7abdcc1f25dcbe6e000857
Tiago Marcial <tiko127@hotmail.com>
2025-07-27 15:01:41 -0300

    Adiciona sombreamento na imagem topo da tela de filmes

--- app/src/main/java/dev/tiagomarcial/netflix_remake/MovieActivity.kt
+++ app/src/main/java/dev/tiagomarcial/netflix_remake/MovieActivity.kt
@@ -1,10 +1,13 @@
 package dev.tiagomarcial.netflix_remake
 
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -20,5 +23,12 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.outline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
     }
 }
